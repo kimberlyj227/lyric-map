36c2ce0c9e74260f723bda050737ef6542b4093d
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-08-26 21:59:33 -0600

    Updates

---
+++ precache-manifest.a47fe9034d3e397ff41ea2f95e7d3cc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd261a82eaf23ce53f5152278097ac63",
+    "url": "/lyric-map/index.html"
+  },
+  {
+    "revision": "81a7a97a7ac35632c13e",
+    "url": "/lyric-map/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "81189f4ce87cdd93aa35",
+    "url": "/lyric-map/static/css/main.5750e805.chunk.css"
+  },
+  {
+    "revision": "81a7a97a7ac35632c13e",
+    "url": "/lyric-map/static/js/2.50b48103.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lyric-map/static/js/2.50b48103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81189f4ce87cdd93aa35",
+    "url": "/lyric-map/static/js/main.c20fc55f.chunk.js"
+  },
+  {
+    "revision": "5c9d219f7b582497c623",
+    "url": "/lyric-map/static/js/runtime-main.073cea42.js"
+  },
+  {
+    "revision": "2e8e1ee3b0f407866451279fb0782cc3",
+    "url": "/lyric-map/static/media/backgroundLyric.2e8e1ee3.jpg"
+  }
+]);
