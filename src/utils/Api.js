6748682f1f721be7d755891ca927ab1c7076b3fb
kimberly227 <kimberly.j.owens@gmail.com>
2024-10-29 14:48:02 -0600

    removing extra / from api

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASEURL = "https://api.lyrics.ovh/v1/";
+const BASEURL = 'https://api.lyrics.ovh/v1';
 
 export default {
-  search: function(artist, title) {
-    return axios.get(`${BASEURL}/${artist}/${title}`);
-  }
+	search: function (artist, title) {
+		return axios.get(`${BASEURL}/${artist}/${title}`);
+	}
 };
